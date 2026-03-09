7bc448cefb35307bd388d22df989e05d6e2e1bd2
Shivangi Pant <shivangi.202104193@tulas.edu.in>
2026-03-09 21:29:58 +0530

    install axios dependency

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,8 +8,7 @@ function Header() {
     <header className='w-full  bg-slate-900 border-slate-800'>
     <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         <Brain className="  text-blue-400 " />
-        
-
+      
         <Navbar/>
     
     </div>    
